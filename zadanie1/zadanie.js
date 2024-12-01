bd8394ffc0a0ddb34a0adf39a4c3afdad030ffaa
Szymon <gutek.gutomski@gmail.com>
2024-12-01 20:29:27 +0100

    if

--- zadanie1/zadanie.js
+++ zadanie1/zadanie.js
@@ -1,3 +1,26 @@
-const proccestext = (text, {uppercase = false, reverse = false, removeSpecialChars = false} = {})
+const processText = (text, {uppercase = false, reverse = false, removeSpecialChars = false} = {}) => {
+    let result = [...text];
+
+if(uppercase){
+    result = result.map(char => char.toUpperCase());
+}
+
+if(reverse){
+    result = result.reverse();
+}
+
+if(removeSpecialChars){
+    result = result.filter(char => /[a-zA-Z0-9]/.test(char));
+}
+
+return result.join('');
+
+}
+
+console.log(processText("Hello, World!", { uppercase: true }));
+console.log(processText("Hello, World!", { reverse: true }));
+console.log(processText("Hello, World!", { removeSpecialChars: true }));
+
+
 
 
