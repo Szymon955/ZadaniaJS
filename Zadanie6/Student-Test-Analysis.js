bf5669c01bf2d3677665767ab758f651177e56dd
Szymon <gutek.gutomski@gmail.com>
2024-12-01 23:15:24 +0100

    Dodano funkcje

--- Zadanie6/Student-Test-Analysis.js
+++ Zadanie6/Student-Test-Analysis.js
@@ -5,3 +5,16 @@ const students = [
     { name: 'Kasia', score: 94 },
     { name: 'Marek', score: 88 }
   ];
+
+  // Funkcja do obliczania średniej
+function calculateAverage(scores) {
+    const total = scores.reduce((acc, score) => acc + score, 0); // Sumowanie wyników
+    return total / scores.length; // Dzielenie
+  }
+  
+  // Funkcja do znajdowania najwyższego i najniższego wyniku
+  function findMinMax(scores) {
+    const max = Math.max(...scores);
+    const min = Math.min(...scores); 
+    return { max, min };
+  }
