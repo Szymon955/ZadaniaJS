e48f3dfd6de066fc9685d45c6a37de580cd6f6ec
Szymon <gutek.gutomski@gmail.com>
2024-12-01 21:45:40 +0100

    Stworzenie klasy i metod

--- Zadanie3/Student-Grade-Manager.js
+++ Zadanie3/Student-Grade-Manager.js
@@ -1,3 +1,17 @@
 class Student{
-    
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.grades = []; //lista ocen
+    }
+
+    addGrade(grade){
+        this.grades = [...this.grades, grade];
+    }
+
+    getAverage(){
+        if(this.grades.length === 0) return 0;
+        const total = this.grades.reduce((sum, grade) => sum + grade, 0);
+        return total / this.grades.length;
+    }
 }
