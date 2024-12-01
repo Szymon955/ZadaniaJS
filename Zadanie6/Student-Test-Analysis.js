199e24e60b1e4a098d0fcc2b9a6535827cb3946c
Szymon <gutek.gutomski@gmail.com>
2024-12-01 23:10:02 +0100

    Utworzenie tablicy

---
+++ Zadanie6/Student-Test-Analysis.js
@@ -0,0 +1,7 @@
+const students = [
+    { name: 'Jan', score: 85 },
+    { name: 'Anna', score: 92 },
+    { name: 'Piotr', score: 78 },
+    { name: 'Kasia', score: 94 },
+    { name: 'Marek', score: 88 }
+  ];
